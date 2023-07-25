991888dc92677132c94785be993975c96537cf0c
dishb <code.dishb@gmail.com>
2023-07-25 13:54:39 -0700

    feat: add license argument (cli)

--- monstera/__init__.py
+++ monstera/__init__.py
@@ -177,25 +177,37 @@ def _main() -> int:
                                   and location.""",
                         required = False,
                         dest = "names",
-                        metavar = "MODULE NAME"
+                        metavar = "MODULE NAMES"
                         )
+    parser.add_argument("-c", "--copyright",
+                        nargs = 0,
+                        action = "store",
+                        help = "display monstera's copyright (from the MIT License)",
+                        required = False,
+                        dest = "copyright",
+                        )
+
     args = parser.parse_args()
 
     packages = args.names
+    copyright_ = args.copyright
     info = run(packages = packages)
 
-    print(f"\nPython: {info['python_version']}, {info['release_level']}")
-    print(f"\nPython Location: {info['python_location']}")
-    print(f"\nOperating System: {info['os']} {info['os_version']}")
-    print(f"\nArchitecture: {info['architecture']}")
-    print(f"\nPip: {info['pip_version']}")
-    print(f"\nPip Location: {info['pip_location']}")
+    if copyright_ is not None:
+        print(f"\nPython: {info['python_version']}, {info['release_level']}")
+        print(f"\nPython Location: {info['python_location']}")
+        print(f"\nOperating System: {info['os']} {info['os_version']}")
+        print(f"\nArchitecture: {info['architecture']}")
+        print(f"\nPip: {info['pip_version']}")
+        print(f"\nPip Location: {info['pip_location']}")
 
-    if packages is not None:
-        for pkg in packages:
-            print(f"\n{pkg}:")
-            print(f"    Location: {info[f'{pkg}_location']}")
-            print(f"    Version: {info[f'{pkg}_version']}")
+        if packages is not None:
+            for pkg in packages:
+                print(f"\n{pkg}:")
+                print(f"    Location: {info[f'{pkg}_location']}")
+                print(f"    Version: {info[f'{pkg}_version']}")
+    else:
+        print(__copyright__)
 
     print("")
 
